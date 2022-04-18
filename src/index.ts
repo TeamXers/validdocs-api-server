7552ade9a531beafca7c5ffe2477dc15e2eb86d7
Eunovo <eunovo9@gmail.com>
2022-04-18 11:14:44 +0100

    Auto index

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,9 @@ essentials.map((value) => {
     throw new Error(`${value} not set in env!`);
 });
 
-connect(process.env.DB_URI!, () => {
+connect(process.env.DB_URI!, {
+    autoIndex: true
+}, () => {
     console.log(`Connected to DB at ${process.env.DB_URI}`);
 });
 app.listen(process.env.PORT, () => {
