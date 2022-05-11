546d59cffcc4813475db2d122bd41d702700ef1f
Eunovo <eunovo9@gmail.com>
2022-05-11 13:36:39 +0100

    Make address search case-insensitive

--- src/features/accounts/AccountService.ts
+++ src/features/accounts/AccountService.ts
@@ -1,4 +1,5 @@
-import { service, CRUDService, inject, Observable, timestamp } from "@eunovo/superbackend";
+import { service, CRUDService, inject, Observable, timestamp, FilterOptions } from "@eunovo/superbackend";
+import { Filter } from "@eunovo/superbackend/dist/crud/Filter";
 import { Account } from "./AccountModel";
 import { AccountRepo } from "./AccountRepo";
 
@@ -10,5 +11,12 @@ export class AccountService extends CRUDService<Account> {
         @inject(AccountRepo) repo: AccountRepo
     ) {
         super(observable.getObservableFor('accounts'), repo);
+    }
+
+    findMany(filter: Filter<Account>, options?: FilterOptions | undefined, context?: any) {
+        if (filter.address && filter.address.charAt) {
+            filter.address = { $regex: filter.address, $options: 'i' };
+        }
+        return super.findMany(filter, options, context);
     }
 }
