4e0881bddcf92502e1a5ac6acbea833f30ce8b68
Eunovo <eunovo9@gmail.com>
2022-05-29 04:32:10 +0100

    Implement GET /docs/shared

--- src/features/documents/DocController.ts
+++ src/features/documents/DocController.ts
@@ -1,14 +1,29 @@
 import { controller, CRUDController, get, inject } from "@eunovo/superbackend";
 import { InvitesService } from "../invitations/InviteService";
 import { DocService } from "./DocService";
+import { ViewersService } from "./viewers/service";
 
 @controller()
 export class DocController extends CRUDController {
     constructor(
         @inject(DocService) private documents: DocService,
-        @inject(InvitesService) private invites: InvitesService
+        @inject(InvitesService) private invites: InvitesService,
+        @inject(ViewersService) private viewers: ViewersService
     ) {
         super('/docs', documents);
+    }
+
+    @get('/shared')
+    async getShared(req: any) {
+        const address = req.user.address;
+        const shared = await this.viewers.findMany({ userAddress: address });
+        const docs = await (Promise.all(
+            shared.map(value => this.documents.findOne({ tokenId: value.documentTokenId }))
+        ));
+        return {
+            message: 'success',
+            date: { results: docs }
+        }
     }
 
     @get('/:tokenId/sign-invite')
